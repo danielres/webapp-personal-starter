f9b9a1ec29810215efadd23af626865f0b2ed3e1
Daniel Reszka <dannn.r@gmail.com>
2020-12-22 22:03:12 +0100

    convert all files to typescript

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
+}
