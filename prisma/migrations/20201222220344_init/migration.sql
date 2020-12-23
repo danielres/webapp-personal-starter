56cc960c0b1308e3725c2d987883997efa1aef30
Daniel Reszka <dannn.r@gmail.com>
2020-12-23 05:25:13 +0100

    db: User: add timestamps + isSuperUser

--- prisma/migrations/20201222220344_init/migration.sql
+++ prisma/migrations/20201222220344_init/migration.sql
@@ -4,7 +4,10 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-
+    "isSuperUser" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    
     PRIMARY KEY ("id")
 );
 
