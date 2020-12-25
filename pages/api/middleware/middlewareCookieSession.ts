9f3fad623ae2b0cf577d3f702a6670d1907ae9da
Daniel Reszka <dannn.r@gmail.com>
2020-12-25 15:03:08 +0100

    middlewareCookieSession: fix "config"  import not found in tests

--- pages/api/middleware/middlewareCookieSession.ts
+++ pages/api/middleware/middlewareCookieSession.ts
@@ -1,5 +1,5 @@
-import * as config from "config"
 import CookieSession from "cookie-session"
+import * as config from "../../../config"
 import { Request, Response } from "../graphql"
 
 export const middlewareCookieSession = (req: Request, res: Response) => {
