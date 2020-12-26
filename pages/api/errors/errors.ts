ddab440a2759062ae530240c80a164dec503fef3
Daniel Reszka <dannn.r@gmail.com>
2020-12-26 19:26:08 +0100

    api: errors: remove leftover file

--- pages/api/errors/errors.ts
+++
@@ -1,8 +0,0 @@
-export const codes = {
-  prisma: {
-    UNIQUE_VALIDATION_FAILURE: "P2002",
-  },
-}
-
-// Used for unknown server-side errors. The arbitrary id allows for easier debugging:
-export const ServerError = (id: string) => new Error(`Server error #${id}`)
