ba1b442dc24b7bbca0fef4e0e87939610947fdf9
Daniel Reszka <dannn.r@gmail.com>
2020-12-28 21:26:01 +0100

    jest: config: make verbose

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: "ts-jest",
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   testEnvironment: "./jest/CustomEnvironment",
+  verbose: true,
 }
