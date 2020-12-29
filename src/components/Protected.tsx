bc3375eeacd83a50fe858823e47388e96a948765
Daniel Reszka <dannn.r@gmail.com>
2020-12-30 00:29:08 +0100

    ui: Protected: improve with ui & server side validations

--- src/components/Protected.tsx
+++ src/components/Protected.tsx
@@ -1,19 +1,27 @@
+import React, { useState } from "react"
+import { FormProvider, useForm } from "react-hook-form"
 import { SigninMutationVariables } from "src/generated/operations"
-import React from "react"
-import { useForm } from "react-hook-form"
 import { sdk } from "../../sdk"
+import { ErrorResponse, ServerErrorResponse } from "./ui/forms/ErrorResponse"
+import { InputEmail } from "./ui/forms/InputEmail"
+import { InputPassword } from "./ui/forms/InputPassword"
 
 type ProtectedProps = {
   children: React.ReactNode
 }
 
 export default function Protected({ children }: ProtectedProps) {
-  const { handleSubmit, register } = useForm()
+  const formMethods = useForm()
   const { data, error, revalidate } = sdk.useMe()
+  const [errorResponse, setErrorResponse] = useState<ServerErrorResponse>()
 
   const onSubmit = async (vars: SigninMutationVariables) => {
-    await sdk.Signin(vars)
-    await revalidate()
+    try {
+      await sdk.Signin(vars)
+      await revalidate()
+    } catch (error) {
+      setErrorResponse(error)
+    }
   }
 
   const isLoading = !data && !error
@@ -23,23 +31,24 @@ export default function Protected({ children }: ProtectedProps) {
   if (data?.me) return <>{children}</>
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <input name="email" ref={register} placeholder="email" />
-      </div>
+    <>
+      <ErrorResponse response={errorResponse} />
 
-      <div>
-        <input
-          name="password"
-          placeholder="password"
-          ref={register}
-          type="password"
-        />
-      </div>
+      <FormProvider {...formMethods}>
+        <form onSubmit={formMethods.handleSubmit(onSubmit)}>
+          <div>
+            <InputEmail />
+          </div>
 
-      <div>
-        <button type="submit">Sign in</button>
-      </div>
-    </form>
+          <div>
+            <InputPassword />
+          </div>
+
+          <div>
+            <button type="submit">Sign in</button>
+          </div>
+        </form>
+      </FormProvider>
+    </>
   )
 }
