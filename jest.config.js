8a92615d8aae19e4b745944a6f1a8209a3c12db5
Daniel Reszka <dannn.r@gmail.com>
2020-12-31 16:54:35 +0100

    jest: ignore some folders

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   preset: "ts-jest",
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/.github/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/coverage/",
+    "<rootDir>/.vscode/",
+  ],
   testEnvironment: "./jest/CustomEnvironment",
   verbose: true,
   collectCoverage: true,
