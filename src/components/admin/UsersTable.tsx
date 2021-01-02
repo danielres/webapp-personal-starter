a4c10e630dd4a27b2e2495af372f0762e0d50214
Daniel Reszka <dannn.r@gmail.com>
2021-01-02 21:17:47 +0100

    ui: UsersTable: use TableOuter + cleanup

--- src/components/admin/UsersTable.tsx
+++ src/components/admin/UsersTable.tsx
@@ -1,7 +1,11 @@
-import React from "react"
+import classnames from "classnames"
 import Link from "next/link"
+import React from "react"
 import { sdk } from "../../../sdk"
 import { User as TUser } from "../../generated/operations"
+import { Button } from "../ui/Button"
+import { TableOuter } from "../ui/TableOuter"
+import ui from "../ui/ui.module.css"
 
 export function UsersTable() {
   const { data, error } = sdk.useUsers()
@@ -10,23 +14,24 @@ export function UsersTable() {
 
   if (!data?.users) return <div>Loading...</div>
 
+  const headers = [
+    "id",
+    "name",
+    "email",
+    "isSuperUser",
+    "created at",
+    "updated at",
+    "actions",
+  ]
+
   return (
-    <table width="100%">
-      <thead>
-        <th>id</th>
-        <th>name</th>
-        <th>email</th>
-        <th>isSuperUser</th>
-        <th>created at</th>
-        <th>updated at</th>
-        <th>actions</th>
-      </thead>
-      <tbody>{data.users.map((user) => user && <UserRow user={user} />)}</tbody>
-    </table>
+    <TableOuter headers={headers}>
+      {data.users.map((user) => user && <RowUser key={user.id} user={user} />)}
+    </TableOuter>
   )
 }
 
-function UserRow({ user }: { user: TUser }) {
+function RowUser({ user }: { user: TUser }) {
   return (
     <tr>
       <td>{user.id}</td>
@@ -36,7 +41,11 @@ function UserRow({ user }: { user: TUser }) {
       <td>{user.createdAt}</td>
       <td>{user.updatedAt}</td>
       <td>
-        <Link href={`/admin/user/${user.id}`}>edit</Link>
+        <Link href={`/admin/user/${user.id}`} passHref>
+          <Button as="a" variant="action">
+            edit
+          </Button>
+        </Link>
       </td>
     </tr>
   )
