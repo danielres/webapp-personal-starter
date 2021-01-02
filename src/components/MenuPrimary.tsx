0e34db22394e44cf10d3ab002f6d9a5926fad5b4
Daniel Reszka <dannn.r@gmail.com>
2021-01-02 11:00:34 +0100

    ui: MenuPrimary: add basic styles

--- src/components/MenuPrimary.tsx
+++ src/components/MenuPrimary.tsx
@@ -1,4 +1,6 @@
+import classnames from "classnames"
 import Link from "next/link"
+import { useRouter } from "next/router"
 import React from "react"
 import { sdk } from "../../sdk"
 import { SuperUserOnly } from "./SuperUserOnly"
@@ -16,27 +18,94 @@ export default function MenuPrimary() {
   const { me } = data
 
   return (
-    <nav>
-      <Link href="/">Home</Link>
+    <>
+      <div className="bg-white shadow ">
+        <div className="container flex justify-between mx-auto">
+          <nav>
+            <ul className="flex list-none">
+              <li>
+                <Link href="/" passHref>
+                  <A>Home</A>
+                </Link>
+              </li>
+            </ul>
+          </nav>
 
-      <Separator />
+          <nav>
+            <ul className="flex list-none">
+              <li>
+                <Link href="/" passHref>
+                  <T>{me.email}</T>
+                </Link>
+              </li>
 
-      <SuperUserOnly>
-        <>
-          <Link href="/admin">Admin</Link>
-          <Separator />
-        </>
-      </SuperUserOnly>
+              <SuperUserOnly>
+                <li>
+                  <Link href="/admin" passHref>
+                    <A>Admin</A>
+                  </Link>
+                </li>
+              </SuperUserOnly>
 
-      <span>{me.email}</span>
-
-      <Separator />
-
-      <button onClick={signout}>Sign out</button>
-    </nav>
+              <li>
+                <B onClick={signout}>Sign out</B>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </>
   )
 }
 
-function Separator() {
-  return <span>{" | "}</span>
+type TProps = {
+  children: React.ReactNode
+}
+
+function T({ children }: TProps) {
+  return (
+    <span className={classnames("inline-block px-3 py-2 text-gray-500")}>
+      {children}
+    </span>
+  )
+}
+
+type AProps = {
+  children: React.ReactNode
+  href?: string
+  rest?: any
+}
+
+function A({ children, href, ...rest }: AProps) {
+  const router = useRouter()
+
+  const isActive = router.pathname === href
+  return (
+    <a
+      className={classnames(
+        "inline-block px-3 py-2 text-gray-500 hover:text-gray-900  border-b-2 border-transparent",
+        { "border-gray-400 text-gray-900": isActive }
+      )}
+      href={href}
+      {...rest}
+    >
+      {children}
+    </a>
+  )
+}
+
+type BProps = {
+  children: React.ReactNode
+  onClick: () => void
+}
+
+function B({ children, onClick }: BProps) {
+  return (
+    <button
+      className="px-3 py-2 text-gray-500 hover:text-gray-900"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  )
 }
