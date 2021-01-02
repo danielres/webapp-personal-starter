af21bd926e403e750d15aa3181eefb66395e60de
Daniel Reszka <dannn.r@gmail.com>
2021-01-02 22:20:24 +0100

    ui: UsersTable: styles: improve

--- src/components/admin/UsersTable.tsx
+++ src/components/admin/UsersTable.tsx
@@ -1,11 +1,10 @@
-import classnames from "classnames"
 import Link from "next/link"
 import React from "react"
 import { sdk } from "../../../sdk"
 import { User as TUser } from "../../generated/operations"
 import { Button } from "../ui/Button"
 import { TableOuter } from "../ui/TableOuter"
-import ui from "../ui/ui.module.css"
+import { Time } from "../ui/Time"
 
 export function UsersTable() {
   const { data, error } = sdk.useUsers()
@@ -18,7 +17,7 @@ export function UsersTable() {
     "id",
     "name",
     "email",
-    "isSuperUser",
+    "Superuser",
     "created at",
     "updated at",
     "actions",
@@ -38,8 +37,12 @@ function RowUser({ user }: { user: TUser }) {
       <td>{user.name}</td>
       <td>{user.email}</td>
       <td>{user.isSuperUser.toString()}</td>
-      <td>{user.createdAt}</td>
-      <td>{user.updatedAt}</td>
+      <td>
+        <Time time={user.createdAt} />
+      </td>
+      <td>
+        {user.createdAt !== user.updatedAt && <Time time={user.updatedAt} />}
+      </td>
       <td>
         <Link href={`/admin/user/${user.id}`} passHref>
           <Button as="a" variant="action">
