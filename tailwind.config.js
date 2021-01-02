7c69641aa9c564022368aee2256538e3b763e2d5
Daniel Reszka <dannn.r@gmail.com>
2021-01-02 07:50:13 +0100

    ui: setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.[jt]sx?"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
