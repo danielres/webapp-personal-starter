7cb7202efd07af9cb79b637f651c025d2dd863ca
Daniel Reszka <dannn.r@gmail.com>
2021-01-02 05:22:39 +0100

    api: signin: add input validation tests

---
+++ src/api/schema/resolvers/mutations/signin.test.ts
@@ -0,0 +1,21 @@
+/** @usePrisma */
+import { TestSdk } from "../../../test/TestSdk"
+
+const sdk = TestSdk()
+
+describe("Mutation signin", () => {
+  describe("Validations", () => {
+    it("requires valid email + password", async () => {
+      const res: any = await sdk.Signin({ email: "X", password: "X" })
+
+      expect(res.data.signin).toEqual(null)
+      expect(res.errors[0].extensions.exception.message).toMatch(
+        "Validation errors"
+      )
+      expect(res.errors[0].extensions.exception.messages).toMatchObject([
+        "email: should be a valid email",
+        "password: should be between 8 and 64 characters",
+      ])
+    })
+  })
+})
