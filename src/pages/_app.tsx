b25b123aaf05ac9fd285c777fee643d5f67b3493
Daniel Reszka <dannn.r@gmail.com>
2021-01-02 08:00:11 +0100

    ui: import tailwind base styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app"
 import MenuPrimary from "../components/MenuPrimary"
 import Protected from "../components/Protected"
+import "../styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
