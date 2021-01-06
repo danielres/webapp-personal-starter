974fa0084bb5fed8a0a25238f5768aa2cbdca890
Daniel Reszka <dannn.r@gmail.com>
2021-01-06 23:47:13 +0100

    ui: Card + Stack: augment with prop className

--- src/components/ui/Stack.tsx
+++ src/components/ui/Stack.tsx
@@ -2,8 +2,9 @@ import ui from "./ui.module.css"
 
 type StackProps = {
   children: React.ReactNode
+  className?: string
 }
 
-export function Stack({ children }: StackProps) {
-  return <div className={ui.Stack}>{children}</div>
+export function Stack({ children, className }: StackProps) {
+  return <div className={`${ui.Stack} ${className}`}>{children}</div>
 }
