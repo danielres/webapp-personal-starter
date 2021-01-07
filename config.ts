a7af9b0951894ae2a3559535f0b2f194d2104886
Daniel Reszka <dannn.r@gmail.com>
2021-01-07 12:37:41 +0100

    config: export isDev + isProd + isTest

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
-const isDev = process.env.NODE_ENV === "development"
-const isProd = process.env.NODE_ENV === "production"
-const isTest = process.env.NODE_ENV === "test"
+export const isDev = process.env.NODE_ENV === "development"
+export const isProd = process.env.NODE_ENV === "production"
+export const isTest = process.env.NODE_ENV === "test"
 
 export const bcrypt = {
   saltRounts: isProd ? 10 : 1,
