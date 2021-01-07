aeb0dfee0e04888e57a15e043749a700de361096
Daniel Reszka <dannn.r@gmail.com>
2021-01-07 11:23:46 +0100

    ui: Stack: allow custom spacing

--- src/components/ui/Stack.tsx
+++ src/components/ui/Stack.tsx
@@ -1,10 +1,31 @@
-import ui from "./ui.module.css"
-
 type StackProps = {
   children: React.ReactNode
   className?: string
+  spacing?: "xs" | "sm" | "md" | "lg"
 }
 
-export function Stack({ children, className }: StackProps) {
-  return <div className={`${ui.Stack} ${className}`}>{children}</div>
+export function Stack({ children, className, spacing = "md" }: StackProps) {
+  return (
+    <>
+      <div className={`stack-${spacing} ${className}`}>{children}</div>
+
+      <style jsx>{`
+        .stack-xs :global(> * + *) {
+          margin-top: 0.5rem;
+        }
+
+        .stack-sm :global(> * + *) {
+          margin-top: 1rem;
+        }
+
+        .stack-md :global(> * + *) {
+          margin-top: 1.5rem;
+        }
+
+        .stack-lg :global(> * + *) {
+          margin-top: 2rem;
+        }
+      `}</style>
+    </>
+  )
 }
