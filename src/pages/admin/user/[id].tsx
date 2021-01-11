3c0c19663135a39b3a9b594c2380bc10da2b65b4
Daniel Reszka <dannn.r@gmail.com>
2021-01-11 17:32:34 +0100

    ui: admin: UserEditor: replace button cancel with CardLinkBack

--- src/pages/admin/user/[id].tsx
+++ src/pages/admin/user/[id].tsx
@@ -1,12 +1,19 @@
 import { useRouter } from "next/router"
+import React from "react"
 import { sdk } from "../../../../sdk"
 import { FormUser } from "../../../components/admin/forms/FormUser"
 import { Card } from "../../../components/ui/Card"
+import { Spinner } from "../../../components/ui/Spinner"
+import { Stack } from "../../../components/ui/Stack"
+import { CardLinkBack } from "../../../components/ui/CardLinkBack"
 
 export default function UserEditAsAdminById() {
   return (
     <Card className="animate-fadein-fast">
-      <UserEditor />
+      <Stack>
+        <CardLinkBack href={"/admin"} />
+        <UserEditor />
+      </Stack>
     </Card>
   )
 }
@@ -17,7 +24,7 @@ function UserEditor() {
   const id = Number(router.query.id)
   const { data } = sdk.useUser({ id })
 
-  if (!data?.user) return null
+  if (!data?.user) return <Spinner />
 
   const onSuccess = () => router.push("/admin")
 
