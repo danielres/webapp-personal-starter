ca3430fcce52412147c5aed86739adcf10edb695
Daniel Reszka <dannn.r@gmail.com>
2021-01-20 15:31:53 +0100

    db: add Project/User membership relation

--- prisma/migrations/20201222220344_init/migration.sql
+++ prisma/migrations/20201222220344_init/migration.sql
@@ -24,14 +24,32 @@ CREATE TABLE "Project" (
     PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "_projectMemberships" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Project.name_unique" ON "Project"("name");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "_projectMemberships_AB_unique" ON "_projectMemberships"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_projectMemberships_B_index" ON "_projectMemberships"("B");
+
 -- AddForeignKey
 ALTER TABLE "User" ADD FOREIGN KEY("approvedById")REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Project" ADD FOREIGN KEY("ownerId")REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_projectMemberships" ADD FOREIGN KEY("A")REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_projectMemberships" ADD FOREIGN KEY("B")REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/test/Project.test.ts
+++ prisma/test/Project.test.ts
@@ -4,12 +4,16 @@ import type { JestGlobal } from "../../jest/types.d"
 declare const global: JestGlobal
 const { prisma } = global
 
-const ownerData = { name: "owner", email: "owner@example.com", password: "xxx" }
+const owner1Data = { name: "o1", email: "o1@example.com", password: "xxx" }
+const owner2Data = { name: "o2", email: "o2@example.com", password: "xxx" }
+
+const user1Data = { email: "u1@example.com", name: "u1", password: "xxx" }
+const user2Data = { email: "u2@example.com", name: "u2", password: "xxx" }
 
 describe("Project", () => {
   describe("Relations", () => {
     it("has an owner user", async () => {
-      const owner = await prisma.user.create({ data: ownerData })
+      const owner = await prisma.user.create({ data: owner1Data })
 
       const p1 = await prisma.project.create({
         data: { name: "p1", ownerId: owner.id },
@@ -22,7 +26,7 @@ describe("Project", () => {
       })
 
       const userWithProjects = await prisma.user.findUnique({
-        where: { email: ownerData.email },
+        where: { email: owner1Data.email },
         include: { projectsAsOwner: true },
       })
 
@@ -36,6 +40,69 @@ describe("Project", () => {
         { name: "p1" },
         { name: "p2" },
       ])
+    })
+
+    it("has user memberships", async () => {
+      const owner = await prisma.user.create({ data: owner2Data })
+      const u1 = await prisma.user.create({ data: user1Data })
+      const u2 = await prisma.user.create({ data: user2Data })
+
+      const p3 = await prisma.project.create({
+        data: { name: "p3", ownerId: owner.id },
+      })
+      const p4 = await prisma.project.create({
+        data: { name: "p4", ownerId: owner.id },
+      })
+
+      const p3WithMembers = await prisma.project.update({
+        where: { name: "p3" },
+        data: {
+          members: {
+            connect: [{ email: user1Data.email }, { email: user2Data.email }],
+          },
+        },
+        include: { members: true },
+      })
+      const p4WithMember = await prisma.project.update({
+        where: { name: "p4" },
+        data: {
+          members: {
+            connect: [{ email: user1Data.email }],
+          },
+        },
+        include: { members: true },
+      })
+
+      expect(p3WithMembers).toMatchObject({
+        name: "p3",
+        members: [{ name: "u1" }, { name: "u2" }],
+      })
+      expect(p4WithMember).toMatchObject({
+        name: "p4",
+        members: [{ name: "u1" }],
+      })
+
+      const u1AsMember = await prisma.user.findUnique({
+        where: { email: "u1@example.com" },
+        include: { projectsAsMember: true },
+      })
+
+      const u2AsMember = await prisma.user.findUnique({
+        where: { email: "u2@example.com" },
+        include: { projectsAsMember: true },
+      })
+
+      expect(u1AsMember?.projectsAsMember.length).toEqual(2)
+      expect(u1AsMember).toMatchObject({
+        email: "u1@example.com",
+        projectsAsMember: [{ name: "p3" }, { name: "p4" }],
+      })
+
+      expect(u2AsMember?.projectsAsMember.length).toEqual(1)
+      expect(u2AsMember).toMatchObject({
+        email: "u2@example.com",
+        projectsAsMember: [{ name: "p3" }],
+      })
     })
   })
 })
