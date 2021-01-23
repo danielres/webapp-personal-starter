e73e3c491914f3749ac7b284fe775a4a861d52cc
Daniel Reszka <dannn.r@gmail.com>
2021-01-23 23:39:15 +0100

    scripts: add db:seed

---
+++ prisma/seed.ts
@@ -0,0 +1,56 @@
+import { PrismaClient } from "@prisma/client"
+import bcrypt from "bcrypt"
+import * as config from "../config"
+import { fakeUsersData } from "./seed/fakeUsersData"
+import { allowOnlyIn } from "./utils/allowOnlyIn"
+
+allowOnlyIn("dev", "test", "staging")
+
+const prisma = new PrismaClient()
+
+async function main() {
+  if (process.argv[2] === "purge=true") {
+    await prisma.project.deleteMany({ where: {} })
+    await prisma.user.deleteMany({ where: {} })
+  }
+
+  const password = "big-red-fox"
+  const hashedPassword = await bcrypt.hash(password, config.bcrypt.saltRounts)
+
+  const superuser = await prisma.user.create({
+    data: {
+      email: "superuser@example.com",
+      name: "Superuser",
+      password: hashedPassword,
+      isSuperUser: true,
+      emailVerifiedAt: new Date(),
+    },
+  })
+
+  await prisma.user.update({
+    where: { id: superuser.id },
+    data: { approvedBy: { connect: { id: superuser.id } } },
+  })
+
+  await Promise.all(
+    Array.from(Array(60).keys()).map((i) =>
+      prisma.project.create({
+        data: { name: `Project ${i + 1}`, ownerId: superuser.id },
+      })
+    )
+  )
+
+  for (const u of fakeUsersData) {
+    const data = { name: u.name, email: u.email, password: hashedPassword }
+    await prisma.user.create({ data })
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
